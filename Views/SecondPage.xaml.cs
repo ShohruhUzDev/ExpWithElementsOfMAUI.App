9ba996d3651912868de72218df84367840413d87
ShohruhUzDev <shohruh7464784@mail.ru>
2023-02-12 22:57:03 +0500

    height and weight

--- Views/SecondPage.xaml.cs
+++ Views/SecondPage.xaml.cs
@@ -5,5 +5,12 @@ public partial class SecondPage : ContentPage
 	public SecondPage()
 	{
 		InitializeComponent();
+		Button button = new Button
+		{
+			WidthRequest = 100,
+			HeightRequest = 50,
+			Text = "Click"
+		};
+		Content=new Grid { Children = { button } };
 	}
 }
