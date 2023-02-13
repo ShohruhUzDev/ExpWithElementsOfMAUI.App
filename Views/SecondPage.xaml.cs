c590b6697a9af649c72dbe9ea6d0e3721c2185fd
ShohruhUzDev <shohruh7464784@mail.ru>
2023-02-13 13:21:20 +0500

    add new changes

--- Views/SecondPage.xaml.cs
+++ Views/SecondPage.xaml.cs
@@ -5,5 +5,18 @@ public partial class SecondPage : ContentPage
 	public SecondPage()
 	{
 		InitializeComponent();
-	}
+    }
+
+    private  async void Hello_Pressed(object sender, EventArgs e)
+    {
+        await scroll.ScrollToAsync(0, 2, true);
+    }
+
+    
+
+
+    private void salom_Completed_1(object sender, EventArgs e)
+    {
+        DisplayAlert("hello", "Tugatilganda", "ok");
+    }
 }
